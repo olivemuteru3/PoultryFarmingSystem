672c6788c5551ee8fb8066ef6772b6f4f64532f5
Denno <dellyit001@gmail.com>
2024-02-22 02:15:16 -0800

    homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    //
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        if (Auth::id()) {
+            $usertype = Auth::user()->usertype;
+
+            if ($usertype === 'farmer') {
+                $bidder = auth()->user();
+
+                return view('Admin.index');
+            } else if ($usertype === 'admin') {
+
+
+                return view('Admin.index');
+            }
+        } else {
+            return view('auth.login');
+        }
+    }
 }
