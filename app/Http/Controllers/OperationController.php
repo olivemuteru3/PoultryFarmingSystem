5330ab10c3cadfd8cbe761db761d5a62e48c4864
Denno <dellyit001@gmail.com>
2024-02-26 04:29:22 -0800

    Operational Controller

--- app/Http/Controllers/OperationController.php
+++ app/Http/Controllers/OperationController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chicken;
 use Illuminate\Http\Request;
 
 class OperationController extends Controller
 {
-    //
+    public function RegisterChickens(Request $request)
+    {
+
+        $this -> $request->validate([
+            'name' => 'required|string',
+            'contact_number' => 'required|string',
+            'num_chickens' => 'required|integer',
+            'date' => 'required|date',
+            'comments' => 'required|string',
+        ]);
+        $chicken=new Chicken();
+        $chicken->farmerName=$request->farmerName;
+        $chicken->farmerPhone=$request->farmerPhone;
+        $chicken->number=$request->chicken_number;
+        $chicken->date=$request->date;
+        $chicken->comments=$request->comments;
+
+        return response()->json($chicken);
+
+    }
 }
