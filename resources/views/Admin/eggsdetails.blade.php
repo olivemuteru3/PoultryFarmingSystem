0198799b88b5c05978c0d93453ad4bb555330b1c
olivemuteru3 <olivemuteru@gmail.com>
2024-03-04 18:09:57 +0300

    Eggs view

--- resources/views/Admin/eggsdetails.blade.php
+++ resources/views/Admin/eggsdetails.blade.php
@@ -16,6 +16,8 @@
                     <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                 </div>
                 <div class="ms-3">
+
+                <!-- Auth details -->
                     <h6 class="mb-0">{{auth()->user()->name}}</h6>
                     <span>Farmer</span>
                 </div>
